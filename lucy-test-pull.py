8ba65cbb76ba92e3447b398ca4b3cee098c71550
Lucy Adeniji <lucy.adenii@gmail.com>
2023-12-09 20:08:14 +0000

    Testing push request to my fork

---
+++ lucy-test-pull.py
@@ -0,0 +1,14 @@
+""" 1 Data Science Assessment
+  2 Reproducing a data analysis on the underepresnetation 
+  of women and ethnic in Hollywood's leaing movie casts
+  """
+
+#Import relevant libraries and set defaults 
+import numpy as np
+import pandas as pd
+
+movie = pd.read_csv("/Users/lucyadeniji/Documents/Github/dsi-final/data/movie_dataset.csv")
+
+print("Hi summer, first github push test!")
+#print(movie.head())
+
